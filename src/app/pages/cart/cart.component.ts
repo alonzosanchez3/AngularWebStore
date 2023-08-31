5303bd7a3e394c9fb2e47063d1de734c53b5da30
Alonzo Sanchez <alonzosanchez3@gmail.com>
2023-08-31 01:55:19 -0500

    added cart page

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -15,7 +15,15 @@ export class CartComponent {
     price: 150,
     quantity: 1,
     id: 1
-  }]};
+  },
+  {
+    product: 'https://via.placeholder.com/150',
+    name: 'jordans',
+    price: 250,
+    quantity: 2,
+    id: 2
+  }
+]};
 
   dataSource: CartItem[] = []
   displayedColumns: string[] = [
@@ -26,6 +34,13 @@ export class CartComponent {
     'total',
     'action'
   ]
+
+  getTotal(items: CartItem[]): number {
+    return items.map((item) => {
+      return item.price * item.quantity
+    })
+    .reduce((prev, current) => prev + current, 0)
+  }
 
   ngOnInit() {
     this.dataSource = this.cart.items;
